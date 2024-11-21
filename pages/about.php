5671f8a9dee429e933cf7e88c49a31e1b6882ecc
Nico0105 <nicogabescolar@gmail.com>
2024-11-20 22:51:39 -0300

    aboutcorreccion

--- pages/about.php
+++ pages/about.php
@@ -68,17 +68,36 @@
           </section>
 
           <section>
-            <article>
-              <div>
-                <p>
-                  Nosotros 
-                </p>
-              </div>
-            </article>
+              <article>
+                <div>
+                  <h2>¿Quiénes somos?</h2>
+                  <p>
+                    En <strong>Nexus</strong>, somos mucho más que una tienda de productos gaming; somos un equipo apasionado por los videojuegos y la tecnología. Nos dedicamos a ofrecer los mejores accesorios y productos para gamers que buscan no solo calidad, sino una experiencia de juego inigualable.
+                  </p>
+                  <p>
+                    Sabemos que el mundo de los videojuegos es mucho más que una simple afición: es una forma de vida, una comunidad global y, sobre todo, una pasión que merece ser respaldada por el mejor equipamiento. Por eso, seleccionamos cuidadosamente cada producto que ofrecemos, garantizando solo lo mejor en términos de rendimiento, durabilidad y diseño.
+                  </p>
+                  <h3>Lo que nos diferencia</h3>
+                  <ul>
+                    <li>
+                      <strong>Calidad Premium:</strong> Trabajamos con las marcas más reconocidas del mercado, ofreciéndote productos de gama alta para llevar tu experiencia gaming al siguiente nivel.
+                    </li>
+                    <li>
+                      <strong>Variedad:</strong> Ya sea que busques una nueva PC gamer, un teclado mecánico, auriculares de alta fidelidad, o cualquier otro accesorio, en Nexus tenemos todo lo que necesitas para equiparte como un verdadero profesional.
+                    </li>
+                    <li>
+                      <strong>Atención al cliente personalizada:</strong> Sabemos que cada gamer tiene necesidades únicas, por eso nos esforzamos por brindarte un servicio de atención al cliente excepcional, ayudándote a elegir el producto perfecto para ti.
+                    </li>
+                    <li>
+                      <strong>Envíos rápidos y seguros:</strong> Entendemos que lo que más deseas es disfrutar de tus productos cuanto antes, por lo que te garantizamos envíos rápidos y seguros para que no tengas que esperar mucho para comenzar a jugar.
+                    </li>
+                  </ul>
+                </div>
+              </article>
           </section>
 
 
-
+          
           <!-- Related Products -->
           <section class="row">
             <h2 class="text-center mb-4 mt-5">Related Products</h2>

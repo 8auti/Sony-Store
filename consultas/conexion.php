c581eb9146bbd6c23a0ca425c6dbc61439cb09e7
Steve <estebanhirzfeld@gmail.com>
2024-11-21 14:29:03 -0300

    fix

--- consultas/conexion.php
+++ consultas/conexion.php
@@ -2,7 +2,7 @@
     try{
         $conexion = new PDO('mysql:host=localhost;dbname=tienda_pc', 'root', '');
     }catch(PDOException $e){
-        header("Location: ./pages/error.html");
+        header("Location: /nexus/pages/error.html");
         exit;
     }
 ?>

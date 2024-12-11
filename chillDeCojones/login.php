a68c3fd8a25d467a01ca0985f32f81a704ae2906
8auti <bautistasilvalosilla@gmail.com>
2024-12-11 12:53:28 -0300

    wupsi

--- chillDeCojones/login.php
+++ chillDeCojones/login.php
@@ -5,17 +5,6 @@ require_once('../consultas/consultas_usuarios.php');
 
 $error = null;
 
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-$nombre = $_POST['nombre'] ?? null;
-$nombre = test_input($nombre);
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'] ?? null;
     $contraseña = $_POST['contraseña'] ?? null;

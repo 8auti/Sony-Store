08f83a68e283a06764f3a4e695172c6842bcff2e
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2024-12-11 17:23:53 -0300

    feat: session on conexion

--- consultas/conexion.php
+++ consultas/conexion.php
@@ -1,6 +1,7 @@
 <?php
     try{
         $conexion = new PDO('mysql:host=localhost;dbname=tienda_pc', 'root', '');
+        session_start();
     }catch(PDOException $e){
         header("Location: /nexus/pages/error.html");
         exit;
